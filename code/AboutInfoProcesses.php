91f33713a3f9e457798b199518718a646290b73f
Mark Stephens <mr.morphic@gmail.com>
2016-03-15 18:02:50 +1300

    fix formatting of processes

--- code/AboutInfoProcesses.php
+++ code/AboutInfoProcesses.php
@@ -1,17 +1,26 @@
 <?php
 
-class AboutInfoEnvironment extends AboutInfoProvider {
+class AboutInfoProcesses extends AboutInfoProvider {
 
-	private static $provider_title = 'Environment';
+	private static $provider_title = 'Processes';
 
 	public function render() {
-		return $this->renderWith('AboutInfoEnvironment');
+		return $this->renderWith('AboutInfoProcesses');
 	}
 
 	public function Processes() {
 		// @todo figure how this will behave if exec is disabled. Is there another way to get processes, e.g. via file system?
 		exec('ps aux', $output);
 
-		return print_r($output,true);
+		if (!is_array($output)) {
+			// Not sure what it returned
+			return print_r($output,true);
+		}
+
+		$result = '<br/>';
+		foreach ($output as $row) {
+			$result .= $row . "<br/>";
+		}
+		return $result;
 	}
 }
--- templates/AboutInfoProcesses.ss
+++ templates/AboutInfoProcesses.ss
@@ -1,8 +1,7 @@
-<h1>Environment</h1>
+<h1>Processes</h1>
 
-Note: these details are specific to the web server, and not correlated when there are multiple web servers.
-
-<h2>Current processes</h2>
+Note: these details are specific to the web server that services the request. If you have multiple web servers,
+relying on this as a definitive list of processes is not recommended.
 
 <pre>
 	$Processes
